2b0ef0ed74ae36ee3395b017ceecb152afbfcbfb
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-11-16 22:31:03 +0100

    database helper

--- app/src/main/java/com/example/globomed/DatabaseHelper.kt
+++ app/src/main/java/com/example/globomed/DatabaseHelper.kt
@@ -5,16 +5,18 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class DatabaseHelper(
-    context: Context?,
-    name: String?,
-    factory: SQLiteDatabase.CursorFactory?,
-    version: Int
-) : SQLiteOpenHelper(context, name, factory, version) {
+    context: Context?
+) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     override fun onCreate(db: SQLiteDatabase?) {
         TODO("Not yet implemented")
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         TODO("Not yet implemented")
+    }
+
+    companion object {
+        const val  DATABASE_NAME = "globomed.db"
+        const val DATABASE_VERSION = 1
     }
 }
